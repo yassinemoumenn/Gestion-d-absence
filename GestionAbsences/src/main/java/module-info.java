bee65a9539cbf2a4bbefa69e332c1e3bd1b892f8
khouilid <abdelkbirkhouilid32@gmail.com>
2021-01-16 12:47:25 +0100

    add DB connection

--- GestionAbsences/src/main/java/module-info.java
+++ GestionAbsences/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires mysql.connector.java;
 
     opens org.example to javafx.fxml;
     exports org.example;
