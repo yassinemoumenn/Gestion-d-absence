81f81f1ddd8e2e6882e5dd376723a9056ae7832b
khouilid <abdelkbirkhouilid32@gmail.com>
2021-02-03 09:48:55 +0100

    fix login function

--- Gestion-d-absence-apprenant/GestionAbsences/src/main/java/org/example/App.java
+++ Gestion-d-absence-apprenant/GestionAbsences/src/main/java/org/example/App.java
@@ -13,20 +13,10 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
-    public static void setRoot(String secretaire) {
-
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
 
-//        Parent root = FXMLLoader.load(getClass().getResource("contrapprenant.fxml"));
-//Parent root = FXMLLoader.load(getClass().getResource("AdminPanel.fxml"));
-        /*    Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));*/
-        Parent root = FXMLLoader.load(getClass().getResource("Formateur.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         stage.setTitle("Gestion d'asbcenses");
         stage.setScene(new Scene(root));
 
--- Gestion-d-absence-apprenant/GestionAbsences/src/main/java/org/example/Login.java
+++ Gestion-d-absence-apprenant/GestionAbsences/src/main/java/org/example/Login.java
@@ -2,18 +2,23 @@ package org.example;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import org.example.MysqlConnect.Connexion;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class Login{
+public class Login {
 
     @FXML
     private TextField EmailInput;
@@ -23,38 +28,37 @@ public class Login{
     @FXML
     private Button LoginButton;
 
-    public void login(MouseEvent mouseEvent) throws SQLException {
+    public void login(MouseEvent mouseEvent) throws SQLException, IOException {
 
-        String email = EmailInput.getText() ;
+        String email = EmailInput.getText();
         String password = mdpInput.getText();
         Connection conn = Connexion.connect();
-        PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `users` WHERE email= ? && password = ?");
+        PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `Users` WHERE email= ? && password = ?");
         preparedStatement.setString(1, email);
         preparedStatement.setString(2, password);
-
         ResultSet resultSet = preparedStatement.executeQuery();
 
 
         if
         (resultSet.next()) {
             String type = resultSet.getString("email");
-            System.out.println(email);
-            System.out.println(password);
-            switch (resultSet.getString("type")) {
-                case "Admin":
-                    App.setRoot("admin");
-                    break;
-                case "Secretaire":
-                    App.setRoot("secretaire");
-                    break;
-                case "Formateur":
-                    App.setRoot("formateur");
-                    break;
-                case "Apprenant":
-                    App.setRoot("Apprenant");
-                    break;
-            }
+            Stage stage = new Stage();
+            Parent root;
+            stage.setTitle("Gestion d'asbcenses");
 
+            if (resultSet.getString("type").equals("Admin")) {
+                root = FXMLLoader.load(getClass().getResource("AdminPanel.fxml"));
+                stage.setScene(new Scene(root));
+            }
+            else if (resultSet.getString("type").equals("Formateur")) {
+                root = FXMLLoader.load(getClass().getResource("Formateur.fxml"));
+                stage.setScene(new Scene(root));
+            }
+            else if (resultSet.getString("type").equals("Apprenant")) {
+                root = FXMLLoader.load(getClass().getResource("contrapprenant.fxml"));
+                stage.setScene(new Scene(root));
+            }
+            stage.show();
         }
     }
 }
